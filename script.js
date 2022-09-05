e7092567784aff2a3f729b25c3d34a014331c43c
y.kalitynskyi <yarik1120@gmail.com>
2022-09-05 23:22:15 +0300

    upd

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const downloadBtn = document.querySelector('.download-button')
 const urlInput = document.querySelector('.url-input')
+const errorMsg = document.getElementById('error-message')
+const spinner = document.querySelector('.spinner')
+const URL_EXAMPLE = 'https://www.youtube.com/watch?'
+const URL_DOWNLOAD = 'http://localhost:4000/download?URL='
+
 downloadBtn.addEventListener('click', () => {
-    sendURL(urlInput.value)
+    if (urlInput.value && urlInput.value.includes(URL_EXAMPLE)) {
+        urlInput.classList.remove('error')
+        setStyleElement(errorMsg,'none')
+        sendURL(urlInput.value)
+        setSpinner()
+    } else {
+        setStyleElement(errorMsg,'block')
+        setStyleElement(spinner, 'none')
+        urlInput.classList.add('error')
+    }
 })
+
+function setStyleElement (element, value) {
+    element.style.display = value
+}
+function setSpinner () {
+    setStyleElement(spinner,'block')
+    let timerId = setInterval(() => setStyleElement(spinner,'block'), 1)
+    setTimeout(() => {
+        clearInterval(timerId)
+        setStyleElement(spinner,'none')
+    }, 1400)
+}
 function sendURL(URL) {
-    window.location.href = `http://localhost:4000/download?URL=${URL}`
+    window.location.href = `${URL_DOWNLOAD}${URL}`
 }
