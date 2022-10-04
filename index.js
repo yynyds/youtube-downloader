961a94c790558b501cb345968ea692b2d5dad445
y.kalitynskyi <yarik1120@gmail.com>
2022-10-04 18:04:17 +0300

    fix

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 const ytdl = require('ytdl-core')
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
 const app = express()
 
 app.use(cors())
-app.listen(4000, () => {
-    console.log('Server Works !!! At port 4000')
-})
+
+const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem'))
+}
+
+https.createServer(httpsOptions, app)
+    .listen(4000, () => {
+        console.log('Server Works! At port 4000')
+    })
 app.get('/download', (req,res) => {
     const URL = req.query.URL
     res.header('Content-Disposition', 'attachment; filename="video.mp4"')
